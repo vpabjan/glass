911d91d1b9f037d73805924522484af274056005
Viktor Pabjan <106230644+vpabjan@users.noreply.github.com>
2026-01-18 22:05:24 +0100

    update build script

--- build.sh
+++ build.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-gcc -o glass glass.c -lX11 -std=c23 -D_FORTIFY_SOURCE=2 -O2 -fstack-protector
+FLAGS="-std=c23 \
+-D_FORTIFY_SOURCE=2 \
+-O2 \
+-march=native \
+-fno-plt \
+-fstack-protector-strong \
+-fPIE \
+-pie"
+
+gcc -o glass glass.c -lX11 $FLAGS
+#gcc -o glass-config.c -lncurses $FLAGS
