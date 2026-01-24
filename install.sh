fa5decf3000618fe81132edcb1b0eb83f8c14e6d
Viktor Pabjan <106230644+vpabjan@users.noreply.github.com>
2026-01-24 19:31:52 +0100

    update install.sh

--- install.sh
+++ install.sh
@@ -5,13 +5,19 @@ echo "Building glass"
 chmod +x build.sh
 ./build.sh
 
+# sudo cp extra/glass.desktop /usr/share/xsessions/glass.desktop
+
 echo "Setting up .glass"
 
 mkdir -p ~/.glass
+
 touch ~/.glass/log
 touch ~/.glass/rc.sh
 chmod +x ~/.glass/rc.sh
+
 cp glass ~/.glass/glass
-cp extra/glass.conf ~/.glass/glass.conf
+chmod +x ~/.glass/glass
+
 cp extra/xinitrc ~/.glass/xinitrc
 chmod +x ~/.glass/xinitrc
+cp extra/glass.conf ~/.glass/glass.conf
