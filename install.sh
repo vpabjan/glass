d2b87d052d1f8ebf43ec2a4ce0f49e5e33c58592
Viktor Pabjan <viktor.pabjan@proton.me>
2026-01-31 20:02:13 +0100

    still not ideal

--- install.sh
+++ install.sh
@@ -9,8 +9,8 @@ echo "[*] Installing glass"
 
 # binaries
 
-install -m755 glass "/usr/share/glass"
-install -m755 glassbg "/usr/share/glassbg"
+install -m755 glass "/bin/glass"
+install -m755 glassbg "/bin/glassbg"
 
 install -m644 extra/glass.conf "/etc/glass.conf"
 
@@ -26,8 +26,8 @@ install -m644 extra/xinitrc "$HOME/.glass/xinitrc"
 #install -m644 version "/version"
 
 # scripts
-install -m755 extra/start-glass "/usr/share/start-glass"
-install -m755 extra/update-glass.sh "/usr/share/update-glass"
+install -m755 extra/start-glass "/bin/start-glass"
+install -m755 extra/update-glass.sh "/bin/update-glass"
 #install -m755 extra/update.sh "$LIB/update.sh"
 
 mkdir -p /usr/share/xsessions
