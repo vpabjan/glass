52b339df36126ec98fa363403d5cba885835d424
Viktor Pabjan <viktor.pabjan@proton.me>
2026-02-02 12:15:32 +0100

    XDG_RUNTIME_DIR spec

--- include/env.c
+++ include/env.c
@@ -4,7 +4,7 @@
 
 static inline void init_env(void) {
     setenv("XDG_CURRENT_DESKTOP", "Glass", 1);
-    //setenv("XDG_RUNTIME_DIR", "$HOME", 0);
+    setenv("XDG_RUNTIME_DIR", "$HOME", 0);
     setenv("XDG_SESSION_DESKTOP", "GlassWM", 1);
     setenv("XDG_SESSION_TYPE", "x11", 0);
     setenv("DESKTOP_SESSION", "glass", 1);
